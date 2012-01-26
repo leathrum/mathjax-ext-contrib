d0d45c28020b4a7cbb3afdf6a7802634c92bdf28
Thomas Leathrum <leathrum@jsu.edu>
2012-01-26 10:41:15 -0600

    cs URL in counters.js

--- counters/counters.js
+++ counters/counters.js
@@ -205,5 +205,5 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
   
 }));
 
-MathJax.Ajax.loadComplete("https://raw.github.com/leathrum/mathjax-ext-contrib/master/counters/counters.js");
+MathJax.Ajax.loadComplete("https://cs.jsu.edu/mathjax-ext/github/counters/counters.js");
 
