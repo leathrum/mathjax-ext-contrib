1d3e896affbf89fe3bbbecf6ec2b67e0023d2c85
Thomas Leathrum <leathrum@jsu.edu>
2012-02-02 11:48:18 -0600

    forloop extension script and readme

---
+++ forloop/README.md
@@ -0,0 +1,43 @@
+# Extension: forloop
+
+Implements simple for-loop iteration for MathJax, with special handling for arrays.
+
+# Configuration:
+
+    MathJax.Hub.Config({
+      TeX: {
+        extensions: ["http://cs.jsu.edu/mathjax-ext/github/forloop/forloop.js"]
+      }
+    });
+
+# Usage:
+
+    \forloop[step]{start}{stop}{counter}{code}
+    \ForArray[step]{start}{stop}{counter}{code}
+
+See [sample](http://leathrum.github.com/mathjax-ext-contrib/forloop/sample.html)
+
+The `\forloop` macro does *not* work well with `array` environments, because
+the `&` and `\\` special sequences do not work correctly in the LaTeX code
+block.  For that reason, the `\ForArray` macro is provided -- it formats the
+table based on the loop and constructs its own internal `mtable` for the
+MathJax internal representation directly, with default spacing and styling.
+
+Loops can be nested if needed.  The `\ForArray` macro should not be nested
+to a depth more than 2 -- in this case, the inner loop adds entries to rows
+of the array, and the outer loop adds the rows to the array.
+
+If you want delimiters (such as square brackets or parentheses) around an
+array built with `\ForArray`, these will need to be added explicitly in
+the surrounding LaTeX code.
+
+This extension requires (and loads automatically) the `counters` extension,
+also available in this repository.  If you use a local copy of this
+extension, you will need to decide also whether to use a local copy of
+the `counters` extension, and if so, change the URL in the `Require()`
+method call at the beginning of this extension script.
+
+# History:
+
+*2 Feb 2012* -- Tom Leathrum  
+
---
+++ forloop/forloop.js
@@ -0,0 +1,69 @@
+MathJax.Ajax.Require("http://cs.jsu.edu/mathjax-ext/github/counters/counters.js");
+MathJax.Callback.Queue(
+MathJax.Hub.Register.StartupHook("TeX counters Ready",function () {
+  var VERSION = "1.0";
+
+  var TEX = MathJax.InputJax.TeX;
+  var TEXDEF = TEX.Definitions;
+  var MML = MathJax.ElementJax.mml;
+  TEXDEF.macros.forloop = 'FORLOOP';
+  TEXDEF.macros.ForArray = 'FORARRAY';
+  var row = [], table = [];
+  var forarraydepth = 0;
+
+  TEX.Parse.Augment({
+   
+    //
+    //  Implements \forloop[step]{start}{stop}{ctr}{code}
+    //
+    FORLOOP: function (name) {
+      var stepstr = this.trimSpaces(this.GetBrackets(name)),
+          start  = parseInt(this.trimSpaces(this.GetArgument(name))),
+          stop  = parseInt(this.trimSpaces(this.GetArgument(name))),
+          ctrname  = this.trimSpaces(this.GetArgument(name)),
+          code  = this.trimSpaces(this.GetArgument(name));
+      var step = 1;
+  
+      // may need to revisit this in MJv2.0 with new GetBrackets()
+      if (stepstr !== "") step = parseInt(stepstr);
+      for (var i = start; 
+           i <= stop;
+           i += step) {
+        counterarray[ctrname] = i;
+        this.Push(TEX.Parse(code).mml());        
+      };
+    }, // end FORLOOP function def
+
+    FORARRAY: function (name) {
+      var stepstr = this.trimSpaces(this.GetBrackets(name)),
+          start  = parseInt(this.trimSpaces(this.GetArgument(name))),
+          stop  = parseInt(this.trimSpaces(this.GetArgument(name))),
+          ctrname  = this.trimSpaces(this.GetArgument(name)),
+          code  = this.trimSpaces(this.GetArgument(name));
+      var step = 1;
+      row = []; 
+  
+      // may need to revisit this in MJv2.0 with new GetBrackets()
+      if (stepstr !== "") step = parseInt(stepstr);
+      forarraydepth++;
+      for (var i = start; 
+           i <= stop;
+           i += step) {
+        counterarray[ctrname] = i;
+        row.push(MML.mtd.apply(MML,[TEX.Parse(code).mml()]));        
+      };
+      forarraydepth--;
+      if (forarraydepth==0) {
+        this.Push(MML.mtable.apply(MML,table));
+        table=[];
+      }
+      else  table.push(MML.mtr.apply(MML,row));
+
+    } // end FORARRAY function def
+
+  });  // end TEX.Parse.Augment
+  MathJax.Hub.Startup.signal.Post("TeX forloop Ready");
+}));  // end StartupHook function def
+
+MathJax.Ajax.loadComplete("http://cs.jsu.edu/mathjax-ext/github/forloop/forloop.js");
+
