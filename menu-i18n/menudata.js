89b01b9fa41eb42ae1feeab8c51f428853c60b5c
Thomas Leathrum <leathrum@jsu.edu>
2012-04-07 11:38:55 -0500

    many changes based on suggestions by Davide Cervone

---
+++ menu-i18n/menudata.js
@@ -0,0 +1,12 @@
+// MathJax header and footer lines REQUIRED
+// change only URL in loadComplete() to match this file
+MathJax.Menu.ITEM.prototype.labelTranslations =
+// begin JSON menu label translation data
+{ 
+  "Show Math As"  :  "Replacement Text",
+  "About MathJax" :  "Another Replacement",
+  "133%"          :  "***%%"  
+}
+// end JSON data
+; 
+MathJax.Ajax.loadComplete("menudata.js");
